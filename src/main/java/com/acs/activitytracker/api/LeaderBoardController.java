5e1834bdc17d3bd030c57884f5a17c24ad04374f
mohangandhi.vadapalli <mohangandhi.vadapalli@acsicorp.com>
2022-11-29 23:19:24 +0530

    Added welcome message

--- src/main/java/com/acs/activitytracker/api/LeaderBoardController.java
+++ src/main/java/com/acs/activitytracker/api/LeaderBoardController.java
@@ -4,6 +4,7 @@ import com.acs.activitytracker.dto.LeaderBoardDTO;
 import com.acs.activitytracker.dto.LeaderBoardRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,11 @@ public class LeaderBoardController {
     @PostMapping
     public ResponseEntity<List<LeaderBoardDTO>> getLeaders(@RequestBody LeaderBoardRequestDto leaderBoardRequestDto) {
         return ResponseEntity.ok(getAllLeaders());
+    }
+
+    @GetMapping
+    public String welcome() {
+        return "It's working!!!!";
     }
 
 
